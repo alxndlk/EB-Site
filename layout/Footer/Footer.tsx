f5111153d0dc2773f7be40c2195897020c5056f1
Alex <livyk@merck.com>
2024-11-19 15:24:28 +0100

    005: footer part

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -2,38 +2,57 @@ import styles from './Footer.module.css'
 
 import Link from 'next/link';
 import Image from 'next/image';
+import { FaDiscord, FaTelegram, FaTiktok, FaYoutube } from 'react-icons/fa';
 
 export const Footer = () => { 
     return (
-        <div className={styles.footer_container}>
-            <footer className={styles.footer}>
-                <div className={styles.footer_header}>
+        <footer className={styles.footer}>
+            <div className={styles.footer_content}>
+                <div className={styles.content_logo}>
                     <div className={styles.logo}>
-                            <Image width={48} height={48} src="/logo-top.png" alt=''/>
-                            <Link href="/" className={styles.link}>Эпоха Блоков</Link>
-                        </div>
-                        <ul className={styles.ul}>
-                            <li>
-                                <Link href="/news" className={`${styles.link} relative`}>Обновления<div className={styles.circle}></div></Link>
-                            </li>
-                            <li>
-                                <Link href="/donate" className={styles.link}>Донат</Link>
-                            </li>
-                            <li>
-                                <Link href="https://discord.gg/w3ts4QTB" target='_blank' className={styles.link}>Дискорд</Link>
-                            </li>
-                            <li className={styles.gap_li}>
-                            <Link  className={`${styles.button} 'bg-gray-100 rounded-full `}  passHref href='/Эпоха Блоков.exe'>
-                                Начать играть
-                            </Link>
-                            </li>
-                        </ul>
+                        <Image width={24} height={24} src="/logo-top.png" alt="" className={`${styles.logoImage}`} />
+                        <Link href="/" className={`${styles.link}`}>
+                            Эпоха Блоков
+                        </Link>
                     </div>
-                <div className={styles.titles}>
-                    <h1>NOT OFFICIAL MINECRAFT PRODUCT. NOT APPROVED BY OR ASSOCIATED WITH MOJANG SYNERGIES AB</h1>
-                    <h1>© 2024 Эпоха Блоков. Все права защищены.</h1>
                 </div>
-            </footer>
-        </div>
+                <div className={styles.content}>
+                    <h4>Ресурсы</h4>
+                    <div className='flex flex-col'>
+                        <Link href=''>Серверы</Link>
+                        <Link href=''>Обновления</Link>
+                        <Link href=''>Донат</Link>
+                        <Link href=''>Дискорд</Link>
+                        <Link href=''>Форум</Link>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <h4>Социальные сети</h4>
+                    <div className='flex flex-col'>
+                        <Link href=''><FaTelegram />Telegram</Link>
+                        <Link href=''><FaDiscord />Discrod</Link>
+                        <Link href=''><FaTiktok />TikTok</Link>
+                        <Link href=''><FaYoutube />YouTube</Link>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <h4>Разное</h4>
+                    <div className='flex flex-col'>
+                        <Link href=''>Работа у нас</Link>
+                        <Link href=''>Политика конфиденциальности</Link>
+                        <Link href=''>Политика безопастности платежей</Link>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <div className='flex flex-col'>
+                        <button>Скачать лаунчер</button>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.paragraphs}>
+                <p>© 2024 Эпоха Блоков. Все права защищены.</p>
+                <p>NOT OFFICIAL MINECRAFT PRODUCT. NOT APPROVED BY OR ASSOCIATED WITH MOJANG SYNERGIES AB.</p>
+            </div>  
+        </footer>
     )
 }
