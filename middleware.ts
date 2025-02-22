897c2f3f258cd5485bd1dc6a9aed7457d540f970
Oleksandr Livyk <109311598+alxndlk@users.noreply.github.com>
2025-02-23 00:50:42 +0100

    check if worls

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,3 @@
-import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-const allowedIPs = ["199.83.103.128"];
-
-export function middleware(req: NextRequest) {
-  const clientIP = req.headers.get("x-forwarded-for")?.split(",")[0] || req.ip;
-
-  if (clientIP && !allowedIPs.includes(clientIP)) {
-    return new NextResponse("Access Denied", { status: 403 });
-  }
-
-  return withAuth(req);
-}
+export { default } from "next-auth/middleware";
 
 export const config = { matcher: ["/profile", "/payment"] };
